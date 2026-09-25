7caeb4475975f2a977a0fa8b8eed9720d17b0715
David Phillipus <david.n.phillipus@gmail.com>
2026-09-25 14:16:04 +0200

    Add lesson 5: basics

---
+++ 05_basics.py
@@ -0,0 +1,78 @@
+print("Hello Pyton I hope you are working!")
+if 5 > 2:
+    print("Python will be m  second favorite for sure")
+
+#Commenting in python is really weird
+#Pyhton does not have any syntax for multiline comments 
+
+"""
+or you can write out you multi line 
+commnet in this quotations but this
+ remains strings eitherways
+as lond as the sting is not assigned ay variable
+"""
+
+#Variables
+x= 4
+c = "control"
+y = True
+z= 5.6
+
+print(type(x))
+
+# Single or Double Quotes? 
+# String variables can be declared either by using single or double quotes
+
+# Case Sensitive
+a = 4 
+A = "Sally" #A will not overwrite a
+ 
+
+#Variable Names
+myvar = "John" 
+my_var = "John" 
+_my_var = "John" 
+myVar = "John" 
+MYVAR = "John" 
+myvar2 = "John"
+
+
+
+# Camel Case 
+# Each word, except the first, starts with a capital letter: 
+
+myVariableName = "John" 
+
+
+# Pascal Case 
+# Each word starts with a capital letter: 
+
+MyVariableName = "John" 
+
+# Snake Case 
+# Each word is separated by an underscore character: 
+
+my_variable_name = "John" 
+
+
+# Python Variables - Assign Multiple Values 
+# Many Values to Multiple Variables 
+# Python allows you to assign values to multiple variables in one line: 
+ 
+x, y, z = "Orange", "Banana", "Cherry" 
+print(x) 
+print(y) 
+print(z)
+
+
+# One Value to Multiple Variables
+s = d = "Soba"
+
+# Unpack a Collection 
+# If you have a collection of values in a list, tuple etc. Python allows you to extract the values 
+# into variables. This is called unpacking.
+
+fruits = ["apple", "banana", "cherry"] 
+k, j, h = fruits 
+
+#Very Interesting you can also use that with strings e.g x, y, z = 'Hello World'

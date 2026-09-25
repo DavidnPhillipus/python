762d20e818af4e4b1953a585eb9acafa31279ba9
David Phillipus <david.n.phillipus@gmail.com>
2026-09-25 14:34:26 +0200

    Add lesson 19: math

---
+++ 19_math.py
@@ -0,0 +1,20 @@
+# Python Math 
+# Python has a set of built-in math functions, including an extensive math module, that 
+# allows you to perform mathematical tasks on numbers
+
+
+# The min() and max() functions can be used to find the lowest or highest value in an iterable:  
+x = min(5, 10, 25) 
+y = max(5, 10, 25) 
+
+# The abs() function returns the absolute (positive) value of the specified number:  
+# x = abs(-7.25)The pow(x, y) function returns the value of x to the power of y (xy).
+ox = pow(4, 3)
+
+
+import math 
+x = math.sqrt(64)
+x = math.ceil(1.4) 
+y = math.floor(1.4) 
+
+# The math.pi constant, returns the value of PI (3.14...)

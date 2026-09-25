98bd0939860ea4af8a7149b10162b3465700f311
David Phillipus <david.n.phillipus@gmail.com>
2026-09-25 14:34:25 +0200

    Add lesson 18: arrays

---
+++ 18_arrays.py
@@ -0,0 +1,32 @@
+# You already know we dont have arrays in python
+# Note: Python does not have built-in support for Arrays, but Python Lists can be used instead.
+
+
+cars = [ "bmw m4" , "m4 csl" , "m4 cs" , "m8" , "m8 cs" , "x6" , "x8"]
+# Get the value of the first array item: 
+x = cars[0] 
+cars[0] = "Toyota" # Modifying the array
+
+
+# Looping through an array
+# Add one more element to the cars array: 
+cars.append("BMW m9")
+
+# Delete the second element of the cars array: 
+cars.pop(3)
+
+# Delete the element that has the value "Volvo": 
+cars.remove("r8")
+
+# append(), Adds an element at the end of the list 
+# clear(), Removes all the elements from the list 
+# copy(), Returns a copy of the list 
+# count(), Returns the number of elements with the specified value 
+# extend(), Add the elements of a list (or any iterable), to the end of the current list 
+# index(), Returns the index of the first element with the specified value 
+# insert(), Adds an element at the specified position 
+# pop(), Removes the element at the specified position 
+# remove(), Removes the first item with the specified value 
+# reverse(), Reverses the order of the list 
+# sort(), Sorts the list
+

925f2c2c53cae5cbf5f9c603a070481843da9577
David Phillipus <david.n.phillipus@gmail.com>
2026-09-25 14:36:12 +0200

    Add lesson 25: inheritance

---
+++ 25_inheritance.py
@@ -0,0 +1,21 @@
+# Python Inheritance 
+# Inheritance allows us to define a class that inherits all the methods and properties from 
+# another class.
+
+
+# Create a Parent Class
+
+class Person: 
+    def __init__(self, fname, lname): 
+        self.firstname = fname 
+        self.lastname = lname 
+    def printname(self): 
+        print(self.firstname, self.lastname) 
+
+
+    
+class Student(Person): 
+    def __init__(self, fname, lname, year): 
+        super().__init__(fname, lname) 
+        self.graduationyear = year 
+x = Student("Mike", "Olsen", 2019) 
